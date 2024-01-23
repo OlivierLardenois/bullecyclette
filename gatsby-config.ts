aa1250c795ece91cfa8df531b4f601e25fbbac76
Olivier Lardenois <olardenois@gmail.com>
2024-01-23 07:55:06 +0100

    feat(lang): setup i18n

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -10,6 +10,31 @@ const config: GatsbyConfig = {
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
   plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/locales`,
+        name: `locale`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-i18next`,
+      options: {
+        // localeJsonSourceName: `locale`, // name given to `gatsby-source-filesystem` plugin.
+        languages: [`fr`, `en`],
+        defaultLanguage: `fr`,
+        siteUrl: `https://example.com`,
+        // if you are using trailingSlash gatsby config include it here, as well (the default is 'always')
+        trailingSlash: "always",
+        // you can pass any i18next options
+        i18nextOptions: {
+          interpolation: {
+            escapeValue: false, // not needed for react as it escapes by default
+          },
+          nsSeparator: false,
+        },
+      },
+    },
     "gatsby-plugin-postcss",
     {
       resolve: "gatsby-plugin-google-gtag",
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
+import { graphql, type HeadFC, type PageProps } from "gatsby";
+import { useTranslation } from "gatsby-plugin-react-i18next";
 import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
 
 const IndexPage: React.FC<PageProps> = () => {
+  const { t } = useTranslation();
   return (
     <main>
-      <p>Hello</p>
+      <p>{t("hello")}</p>
     </main>
   );
 };
@@ -12,3 +14,17 @@ const IndexPage: React.FC<PageProps> = () => {
 export default IndexPage;
 
 export const Head: HeadFC = () => <title>Home Page</title>;
+
+export const query = graphql`
+  query ($language: String!) {
+    locales: allLocale(filter: { language: { eq: $language } }) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    }
+  }
+`;
