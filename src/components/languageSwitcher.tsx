97fdeac8af65f938c7d2e7b27308a4b6ecb11993
Olivier Lardenois <olardenois@gmail.com>
2024-01-23 09:19:04 +0100

    fix(lang): use i18n for languageSwitcher images alt

--- src/components/languageSwitcher.tsx
+++ src/components/languageSwitcher.tsx
@@ -1,8 +1,9 @@
 import { StaticImage } from "gatsby-plugin-image";
-import { Link, useI18next } from "gatsby-plugin-react-i18next";
+import { Link, useI18next, useTranslation } from "gatsby-plugin-react-i18next";
 import * as React from "react";
 
 const LanguageSwitcher: React.FC = () => {
+  const { t } = useTranslation();
   const { originalPath } = useI18next();
 
   const classname = "hover:opacity-75";
@@ -12,7 +13,7 @@ const LanguageSwitcher: React.FC = () => {
       <Link to={originalPath} language="fr" className={classname}>
         <StaticImage
           src={`../images/fr-flag.png`}
-          alt={`French flag`}
+          alt={t("common.fr-flag-alt")}
           placeholder="blurred"
           layout="fixed"
           width={32}
@@ -22,7 +23,7 @@ const LanguageSwitcher: React.FC = () => {
       <Link to={originalPath} language="en" className={classname}>
         <StaticImage
           src={`../images/en-flag.png`}
-          alt={`United Kingdom flag`}
+          alt={t("common.en-flag-alt")}
           placeholder="blurred"
           layout="fixed"
           width={32}
