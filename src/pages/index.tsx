de35aae3f45f6a924b8b6e4c8aa06b01c70b06ff
Olivier Lardenois <olardenois@gmail.com>
2024-01-24 22:18:51 +0100

    chore(home): enhance style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -50,75 +50,71 @@ const IndexPage: React.FC<PageProps<Queries.IndexPageQuery>> = ({ data }) => {
 
   return (
     <Layout>
-      <StaticImage
-        src={`../images/partners/blancs-coteaux.png`}
-        alt={t("")}
-        placeholder="blurred"
-        layout="fullWidth"
-        className="max-w-6xl mx-auto"
-      />
-
-      <section className="flex flex-col justify-between max-w-6xl mx-auto">
+      <div className="space-y-12">
         <StaticImage
-          src={`../images/fr-flag.png`}
+          src={`../images/partners/blancs-coteaux.png`}
           alt={t("")}
           placeholder="blurred"
-          layout="constrained"
+          layout="fullWidth"
+          className="max-w-6xl mx-auto"
         />
-        <div className="flex justify-between">
-          <div className="w-1/3">
-            <StaticImage
-              src={`../images/partners/blancs-coteaux.png`}
-              alt={t("")}
-              placeholder="blurred"
-              layout="constrained"
-            />
-            <h2>{t("homePage.presentation.title")}</h2>
-            <p>{t("homePage.presentation.text")}</p>
-          </div>
-          <div className="flex justify-center w-2/3">
-            <StaticImage
-              src={`../images/fr-flag.png`}
-              alt={t("")}
-              placeholder="blurred"
-              layout="constrained"
-            />
-          </div>
-        </div>
-      </section>
-      <section className="flex justify-between max-w-6xl mx-auto">
-        {NAV_PAGES.map(({ href, imgAltKey, imgPath }) => {
-          const node = data.navImages.nodes.find(
-            (node) => node.relativePath === imgPath,
-          );
-          if (!node) return null;
 
-          const image = getImage(node.childImageSharp);
-          return image ? (
-            <Link to={href} className="hover:opacity-75">
-              <Card alt={t(imgAltKey)} image={image} />
-            </Link>
-          ) : null;
-        })}
-      </section>
-      <section className="text-center">
-        <p className="inline-block p-6 rounded-lg bg-white text-cg-red">
-          {t("homePage.placesAvailable")}
-        </p>
-      </section>
-      <section className="flex max-w-6xl mx-auto">
-        <Carousel cards={carouselCards} />
-      </section>
-      <section className="flex flex-col">
-        <h3 className="max-w-6xl mx-auto">{t("homePage.partners")}</h3>
-        <div className="text-center bg-white">
-          <div className="max-w-6xl mx-auto py-8 space-x-8">
-            {partnerLogos.map(({ alt, image }) => {
-              return <GatsbyImage image={image} alt={alt} className="" />;
-            })}
+        <section className="flex flex-col justify-between max-w-6xl mx-auto">
+          <div className="flex justify-between">
+            <div className="w-1/3">
+              <StaticImage
+                src={`../images/partners/blancs-coteaux.png`}
+                alt={t("")}
+                placeholder="blurred"
+                layout="constrained"
+              />
+              <h2>{t("homePage.presentation.title")}</h2>
+              <p>{t("homePage.presentation.text")}</p>
+            </div>
+            <div className="flex justify-center w-2/3">
+              <StaticImage
+                src={`../images/fr-flag.png`}
+                alt={t("")}
+                placeholder="blurred"
+                layout="constrained"
+              />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
+        <section className="flex justify-between max-w-6xl mx-auto">
+          {NAV_PAGES.map(({ href, imgAltKey, imgPath }) => {
+            const node = data.navImages.nodes.find(
+              (node) => node.relativePath === imgPath,
+            );
+            if (!node) return null;
+
+            const image = getImage(node.childImageSharp);
+            return image ? (
+              <Link to={href} className="hover:opacity-75">
+                <Card alt={t(imgAltKey)} image={image} />
+              </Link>
+            ) : null;
+          })}
+        </section>
+        <section className="text-center">
+          <p className="inline-block p-6 rounded-lg bg-white text-cg-red">
+            {t("homePage.placesAvailable")}
+          </p>
+        </section>
+        <section className="flex max-w-6xl mx-auto">
+          <Carousel cards={carouselCards} />
+        </section>
+        <section className="flex flex-col">
+          <h3 className="max-w-6xl mx-auto">{t("homePage.partners")}</h3>
+          <div className="text-center bg-white">
+            <div className="max-w-6xl mx-auto py-8 space-x-8">
+              {partnerLogos.map(({ alt, image }) => {
+                return <GatsbyImage image={image} alt={alt} className="" />;
+              })}
+            </div>
+          </div>
+        </section>
+      </div>
     </Layout>
   );
 };
