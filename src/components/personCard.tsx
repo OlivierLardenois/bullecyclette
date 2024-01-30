f96aaf39308b7ced32ef1a87ee87677dd649e435
elsa <elsa.rault@telecomnancy.net>
2024-01-30 19:37:06 +0100

    feat(personCard): create personCard component

---
+++ src/components/personCard.tsx
@@ -0,0 +1,161 @@
+import { StaticImage } from "gatsby-plugin-image";
+import * as React from "react";
+import ArrowBullet from "./arrowBullet";
+import { useTranslation } from "gatsby-plugin-react-i18next";
+
+export type PersonCardProps = {
+  title: string;
+  alt: string;
+  image: string;
+  attribut: string;
+};
+
+const PersonCard: React.FC<PersonCardProps> = ({
+  title,
+  //alt,
+  //image,
+  attribut,
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <div>
+      <div>
+        <ArrowBullet desc={title} color="black" bgcolor="white" />
+      </div>
+
+      <div className="flex justify-center">
+        <StaticImage
+          src={"../images/artists.png"}
+          alt="ticket"
+          layout="constrained"
+          width={600}
+          height={200}
+        />
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        <div className="mx-2">
+          <div className="grid bg-white w-full items-center justify-center rounded-lg m-auto text-center text-blue-800  text-xl">
+            {t(attribut + ".0.name")}
+          </div>
+
+          <div className="gridw-full items-center justify-center m-auto text-center my-2  text-base">
+            {t(attribut + ".0.desc")}
+          </div>
+
+          <div className="gridw-full items-center justify-center m-auto text-center my-2 text-sm">
+            <a
+              href={t(attribut + ".0.url")}
+              className="text-blue-800 underline underline-offset-auto"
+            >
+              {t(attribut + ".0.shorturl")}
+            </a>
+          </div>
+
+          <div className="gridw-full flex justify-center my-2">
+            <StaticImage
+              src={"../images/jeannick_liebert.jpg"}
+              alt="ticket"
+              layout="constrained"
+              width={300}
+              height={300}
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+
+        <div className="mx-2">
+          <div className="grid bg-white w-full items-center justify-center rounded-lg m-auto text-center text-blue-800  text-xl">
+            {t(attribut + ".1.name")}
+          </div>
+
+          <div className="gridw-full items-center justify-center m-auto text-center my-2  text-base">
+            {t(attribut + ".1.desc")}
+          </div>
+
+          <div className="gridw-full items-center justify-center m-auto text-center my-2 text-sm">
+            <a
+              href={t(attribut + ".1.url")}
+              className="text-blue-800 underline underline-offset-auto"
+            >
+              {t(attribut + ".1.shorturl")}
+            </a>
+          </div>
+
+          <div className="gridw-full flex justify-center my-2">
+            <StaticImage
+              src={"../images/tonneliers.jpg"}
+              alt="ticket"
+              layout="constrained"
+              width={300}
+              height={300}
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+
+        <div className="mx-2">
+          <div className="grid bg-white w-full items-center justify-center rounded-lg m-auto text-center text-blue-800  text-xl">
+            {t(attribut + ".2.name")}
+          </div>
+
+          <div className="gridw-full items-center justify-center m-auto text-center my-2  text-base">
+            {t(attribut + ".2.desc")}
+          </div>
+
+          <div className="gridw-full items-center justify-center m-auto text-center my-2 text-sm">
+            <a
+              href={t(attribut + ".2.url")}
+              className="text-blue-800 underline underline-offset-auto"
+            >
+              {t(attribut + ".2.shorturl")}
+            </a>
+          </div>
+
+          <div className="gridw-full flex justify-center my-2">
+            <StaticImage
+              src={"../images/swing_shady.jpg"}
+              alt="ticket"
+              layout="constrained"
+              width={300}
+              height={300}
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+
+        <div className="mx-2">
+          <div className="grid bg-white w-full items-center justify-center rounded-lg m-auto text-center text-blue-800  text-xl">
+            {t(attribut + ".3.name")}
+          </div>
+
+          <div className="gridw-full items-center justify-center m-auto text-center my-2  text-base">
+            {t(attribut + ".3.desc")}
+          </div>
+
+          <div className="gridw-full items-center justify-center m-auto text-center my-2 text-sm">
+            <a
+              href={t(attribut + ".3.url")}
+              className="text-blue-800 underline underline-offset-auto"
+            >
+              {t(attribut + ".3.shorturl")}
+            </a>
+          </div>
+
+          <div className="gridw-full flex justify-center my-2">
+            <StaticImage
+              src={"../images/bal_jojo.jpg"}
+              alt="ticket"
+              layout="constrained"
+              width={300}
+              height={300}
+              className="rounded-lg"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PersonCard;
--- src/pages/balade.tsx
+++ src/pages/balade.tsx
@@ -7,12 +7,24 @@ import Layout from "../components/layout";
 import { EVENT_DATE, SALES_DATE } from "../lib/const";
 import ArrowBullet from "../components/arrowBullet";
 import { StaticImage } from "gatsby-plugin-image";
+import Video from "../components/video";
+import Commitment from "../components/commitment";
+import PersonCard from "../components/personCard";
 
 const BaladePage: React.FC<PageProps> = () => {
   const { t } = useTranslation();
   return (
     <Layout>
-      <section className="my-10">
+      <section className="max-w-6xl mx-auto my-10">
+        <PersonCard
+          alt=""
+          image={t("artists.image")}
+          title={t("artists.title")}
+          attribut="artists.persons"
+        ></PersonCard>
+      </section>
+
+      <section className="max-w-6xl mx-auto my-10">
         <div className="grid grid-rows-2 grid-flow-col">
           <div className="row-span-4">
             <ArrowBullet
@@ -22,21 +34,21 @@ const BaladePage: React.FC<PageProps> = () => {
             />
             <p className="text-justify">{t("balade.ticketDescription")}</p>
           </div>
-          <div className="col-span-2 row-span-1 mx-20 mt-10">
+          <div className="col-span-2 row-span-1 m-auto">
             <CountdownTimer
               title={t("common.counterLabel")}
               deadline={EVENT_DATE}
             />
           </div>
-          <div className="col-span-2 row-span-1 mx-20">
+          <div className="col-span-2 row-span-1 m-auto">
             <CountdownTimer
               title={t("balade.ticketCounterLabel")}
               deadline={SALES_DATE}
             />
           </div>
-          <div className="col-span-2 row-span-2 mx-20  justify-center">
+          <div className="col-span-2 row-span-2 m-auto  justify-center">
             <StaticImage
-              src={`../images/ticket.png`}
+              src="../images/ticket.png"
               alt="ticket"
               placeholder="blurred"
               width={500}
@@ -45,11 +57,27 @@ const BaladePage: React.FC<PageProps> = () => {
           </div>
         </div>
       </section>
-      <section className="my-10">
+
+      <section className="max-w-6xl mx-auto my-10">
         <ArrowBullet
-          desc={t("balade.commitmentTitle")}
+          desc={t("commitment.title")}
           color="black"
           bgcolor="white"
+        />
+        <Commitment />
+      </section>
+
+      <section className="max-w-6xl mx-auto my-10">
+        <ArrowBullet
+          desc={t("balade.videoTitle")}
+          color="black"
+          bgcolor="white"
+        />
+        <Video
+          videoSrcURL="https://www.youtube.com/embed/gU_hqPdNylk?si=sa61AmrNZUG0nxlI"
+          videoTitle="LSF"
+          height="500"
+          width="80%"
         />
       </section>
     </Layout>
