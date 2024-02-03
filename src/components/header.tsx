6ea4ec6922d84063b4007c1574bcb20fb0f84b08
Olivier Lardenois <olardenois@gmail.com>
2024-02-03 17:10:49 +0100

    chore(header): set logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,25 @@
+import { StaticImage } from "gatsby-plugin-image";
 import { Link, useTranslation } from "gatsby-plugin-react-i18next";
 import * as React from "react";
 
 import LanguageSwitcher from "./languageSwitcher";
 
 const Header: React.FC = () => {
+  const { t } = useTranslation();
+
   return (
     <header className="border-b-2 border-black">
-      <div className="flex justify-between max-w-6xl mx-auto">
-        <p>logo</p>
-        <div>
+      <div className="flex justify-between max-w-7xl mx-auto p-4">
+        <Link to={"/"}>
+          <StaticImage
+            src={`../images/logo.png`}
+            alt={t("common.logo")}
+            placeholder="blurred"
+            layout="fixed"
+            height={100}
+          />
+        </Link>
+        <div className="flex flex-col justify-between">
           <LanguageSwitcher />
           <HeadersLinks />
         </div>
@@ -26,7 +37,10 @@ const HeadersLinks = () => {
     <nav>
       <ul>
         {Pages.map((page) => (
-          <li key={page.key} className="hover:text-mainDarkBrown">
+          <li
+            key={page.key}
+            className="hover:opacity-50 font-veteran-typewriter text-xl"
+          >
             <Link to={page.to}>{t(page.key)}</Link>
           </li>
         ))}
