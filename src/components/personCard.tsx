0186cc92a463fedb228a0f4b734a07654fad8bb4
elsa <elsa.rault@telecomnancy.net>
2024-02-05 22:47:43 +0100

    fix(personCard): change static image to gatsby image

--- src/components/personCard.tsx
+++ src/components/personCard.tsx
@@ -1,157 +1,234 @@
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, IGatsbyImageData, getImage } from "gatsby-plugin-image";
 import * as React from "react";
 import ArrowBullet from "./arrowBullet";
-import { useTranslation } from "gatsby-plugin-react-i18next";
+import { useI18next, useTranslation } from "gatsby-plugin-react-i18next";
+import { graphql, useStaticQuery } from "gatsby";
 
 export type PersonCardProps = {
-  title: string;
-  alt: string;
-  image: string;
-  attribut: string;
+  person: "artists" | "winegrowers";
 };
 
-const PersonCard: React.FC<PersonCardProps> = ({
-  title,
-  //alt,
-  //image,
-  attribut,
-}) => {
+type PersonProps = {
+  alt: string;
+  image: IGatsbyImageData;
+};
+
+const PersonTitle: React.FC<PersonProps> = ({ alt, image }) => {
+  return (
+    <div className="flex justify-center">
+      <GatsbyImage image={image} alt={alt} />
+    </div>
+  );
+};
+
+const PersonImage: React.FC<PersonProps> = ({ alt, image }) => {
+  return (
+    <div className="gridw-full flex justify-center my-2">
+      <GatsbyImage image={image} alt={alt} />
+    </div>
+  );
+};
+
+function customPersonCard(
+  person: "artists" | "winegrowers",
+  title: boolean,
+  imageName?: string,
+) {
+  const data = useStaticQuery<Queries.PersonCardQuery>(graphql`
+    query PersonCard {
+      artistsImageTitle: allFile(
+        filter: {
+          relativePath: {
+            in: ["artists/artists_fr.png", "artists/artists_en.png"]
+          }
+        }
+      ) {
+        nodes {
+          relativePath
+          childImageSharp {
+            gatsbyImageData(width: 600, height: 200, layout: CONSTRAINED)
+          }
+        }
+      }
+      winegrowersImageTitle: allFile(
+        filter: {
+          relativePath: {
+            in: [
+              "winegrowers/winegrowers_fr.png"
+              "winegrowers/winegrowers_en.png"
+            ]
+          }
+        }
+      ) {
+        nodes {
+          relativePath
+          childImageSharp {
+            gatsbyImageData(width: 600, height: 200, layout: CONSTRAINED)
+          }
+        }
+      }
+      artistsImages: allFile(filter: { dir: { regex: "/images/artists/" } }) {
+        nodes {
+          relativePath
+          childImageSharp {
+            gatsbyImageData(width: 300, height: 300, layout: CONSTRAINED)
+          }
+        }
+      }
+      winegrowersImages: allFile(
+        filter: { dir: { regex: "/images/winegrowers/" } }
+      ) {
+        nodes {
+          relativePath
+          childImageSharp {
+            gatsbyImageData(width: 300, height: 300, layout: CONSTRAINED)
+          }
+        }
+      }
+    }
+  `);
+
+  const { language } = useI18next();
+
+  if (title) {
+    const personTitleImageFilter = data[`${person}ImageTitle`].nodes.find(
+      (node) => node.relativePath == `${person}/${person}_${language}.png`,
+    );
+    const personTitleImage = personTitleImageFilter
+      ? getImage(personTitleImageFilter.childImageSharp)
+      : null;
+
+    return personTitleImage ? (
+      <PersonTitle image={personTitleImage} alt="" />
+    ) : (
+      <></>
+    );
+  } else {
+    console.log(imageName);
+    const personImageFilter = data[`${person}Images`].nodes.find(
+      (node) => node.relativePath == `${imageName}`,
+    );
+    const personImage = personImageFilter
+      ? getImage(personImageFilter.childImageSharp)
+      : null;
+
+    return personImage ? <PersonImage image={personImage} alt="" /> : <></>;
+  }
+}
+
+const PersonCard: React.FC<PersonCardProps> = ({ person }) => {
   const { t } = useTranslation();
 
   return (
     <div>
       <div>
-        <ArrowBullet desc={title} color="black" bgcolor="white" />
-      </div>
-
-      <div className="flex justify-center">
-        <StaticImage
-          src={"../images/artists.png"}
-          alt="ticket"
-          layout="constrained"
-          width={600}
-          height={200}
+        <ArrowBullet
+          desc={t(person + ".title")}
+          color="black"
+          bgcolor="white"
         />
       </div>
+      {customPersonCard(person, true)}
+
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         <div className="mx-2">
           <div className="grid bg-white w-full items-center justify-center rounded-lg m-auto text-center text-blue-800  text-xl">
-            {t(attribut + ".0.name")}
+            {t(person + ".persons.0.name")}
           </div>
 
-          <div className="gridw-full items-center justify-center m-auto text-center my-2  text-base">
-            {t(attribut + ".0.description")}
+          <div className="gridw-full items-center justify-center m-auto text-center my-2 text-base whitespace-pre-line">
+            {t(person + ".persons.0.description")}
           </div>
 
           <div className="gridw-full items-center justify-center m-auto text-center my-2 text-sm">
             <a
-              href={t(attribut + ".0.url")}
+              href={t(person + ".persons.0.url")}
               className="text-blue-800 underline underline-offset-auto"
             >
-              {t(attribut + ".0.shorturl")}
+              {t(person + ".persons.0.shorturl")}
             </a>
           </div>
 
-          <div className="gridw-full flex justify-center my-2">
-            <StaticImage
-              src={"../images/jeannick_liebert.jpg"}
-              alt="ticket"
-              layout="constrained"
-              width={300}
-              height={300}
-              className="rounded-lg"
-            />
-          </div>
+          {customPersonCard(
+            person,
+            false,
+            t(person + ".persons.0.image").toString(),
+          )}
         </div>
 
         <div className="mx-2">
           <div className="grid bg-white w-full items-center justify-center rounded-lg m-auto text-center text-blue-800  text-xl">
-            {t(attribut + ".1.name")}
+            {t(person + ".persons.1.name")}
           </div>
 
-          <div className="gridw-full items-center justify-center m-auto text-center my-2  text-base">
-            {t(attribut + ".1.description")}
+          <div className="gridw-full items-center justify-center m-auto text-center my-2 text-base whitespace-pre-line">
+            {t(person + ".persons.1.description")}
           </div>
 
           <div className="gridw-full items-center justify-center m-auto text-center my-2 text-sm">
             <a
-              href={t(attribut + ".1.url")}
+              href={t(person + ".persons.1.url")}
               className="text-blue-800 underline underline-offset-auto"
             >
-              {t(attribut + ".1.shorturl")}
+              {t(person + ".persons.1.shorturl")}
             </a>
           </div>
 
-          <div className="gridw-full flex justify-center my-2">
-            <StaticImage
-              src={"../images/tonneliers.jpg"}
-              alt="ticket"
-              layout="constrained"
-              width={300}
-              height={300}
-              className="rounded-lg"
-            />
-          </div>
+          {customPersonCard(
+            person,
+            false,
+            t(person + ".persons.1.image").toString(),
+          )}
         </div>
 
         <div className="mx-2">
           <div className="grid bg-white w-full items-center justify-center rounded-lg m-auto text-center text-blue-800  text-xl">
-            {t(attribut + ".2.name")}
+            {t(person + ".persons.2.name")}
           </div>
 
-          <div className="gridw-full items-center justify-center m-auto text-center my-2  text-base">
-            {t(attribut + ".2.description")}
+          <div className="gridw-full items-center justify-center m-auto text-center my-2 text-base whitespace-pre-line">
+            {t(person + ".persons.2.description")}
           </div>
 
           <div className="gridw-full items-center justify-center m-auto text-center my-2 text-sm">
             <a
-              href={t(attribut + ".2.url")}
+              href={t(person + ".persons.2.url")}
               className="text-blue-800 underline underline-offset-auto"
             >
-              {t(attribut + ".2.shorturl")}
+              {t(person + ".persons.2.shorturl")}
             </a>
           </div>
 
-          <div className="gridw-full flex justify-center my-2">
-            <StaticImage
-              src={"../images/swing_shady.jpg"}
-              alt="ticket"
-              layout="constrained"
-              width={300}
-              height={300}
-              className="rounded-lg"
-            />
-          </div>
+          {customPersonCard(
+            person,
+            false,
+            t(person + ".persons.2.image").toString(),
+          )}
         </div>
 
         <div className="mx-2">
           <div className="grid bg-white w-full items-center justify-center rounded-lg m-auto text-center text-blue-800  text-xl">
-            {t(attribut + ".3.name")}
+            {t(person + ".persons.3.name")}
           </div>
 
-          <div className="gridw-full items-center justify-center m-auto text-center my-2  text-base">
-            {t(attribut + ".3.description")}
+          <div className="gridw-full items-center justify-center m-auto text-center my-2 text-base whitespace-pre-line">
+            {t(person + ".persons.3.description")}
           </div>
 
           <div className="gridw-full items-center justify-center m-auto text-center my-2 text-sm">
             <a
-              href={t(attribut + ".3.url")}
+              href={t(person + ".persons.3.url")}
               className="text-blue-800 underline underline-offset-auto"
             >
-              {t(attribut + ".3.shorturl")}
+              {t(person + ".persons.3.shorturl")}
             </a>
           </div>
 
-          <div className="gridw-full flex justify-center my-2">
-            <StaticImage
-              src={"../images/bal_jojo.jpg"}
-              alt="ticket"
-              layout="constrained"
-              width={300}
-              height={300}
-              className="rounded-lg"
-            />
-          </div>
+          {customPersonCard(
+            person,
+            false,
+            t(person + ".persons.3.image").toString(),
+          )}
         </div>
       </div>
     </div>
--- src/pages/balade.tsx
+++ src/pages/balade.tsx
@@ -1,81 +1,119 @@
 import { graphql, type HeadFC, type PageProps } from "gatsby";
-import { useTranslation } from "gatsby-plugin-react-i18next";
+import { useI18next, useTranslation } from "gatsby-plugin-react-i18next";
 import * as React from "react";
 import CountdownTimer from "../components/countdownTimer";
 
 import Layout from "../components/layout";
 import { EVENT_DATE, SALES_DATE } from "../lib/const";
 import ArrowBullet from "../components/arrowBullet";
-import { StaticImage } from "gatsby-plugin-image";
+import { GatsbyImage, StaticImage, getImage } from "gatsby-plugin-image";
 import Video from "../components/video";
 import Commitment from "../components/commitment";
 import PersonCard from "../components/personCard";
+import Preparation from "../components/preparation";
+import Card from "../components/card";
+import {
+  BullecycletteSchedule,
+  GuinguetteSmallSchedule,
+} from "../components/schedule";
 
-const BaladePage: React.FC<PageProps> = () => {
+const BaladePage: React.FC<PageProps<Queries.BaladePageQuery>> = ({ data }) => {
   const { t } = useTranslation();
+  const { language } = useI18next();
+
+  const bullecycletteImageFilter = data.bullecycletteImages.nodes.find(
+    (node) => node.relativePath == `common/bullecyclette_${language}.png`,
+  );
+  const bullecycletteImage = bullecycletteImageFilter
+    ? getImage(bullecycletteImageFilter.childImageSharp)
+    : null;
+
+  const stepRoadImageFilter = data.stepRoadImages.nodes.find(
+    (node) => node.relativePath == `road/step_road_${language}.png`,
+  );
+  const stepRoadImage = stepRoadImageFilter
+    ? getImage(stepRoadImageFilter.childImageSharp)
+    : null;
+
+  const kmRoadImageFilter = data.kmRoadImages.nodes.find(
+    (node) => node.relativePath == `road/km_${language}.png`,
+  );
+  const kmRoadImage = kmRoadImageFilter
+    ? getImage(kmRoadImageFilter.childImageSharp)
+    : null;
+
+  const descRoadImageFilter = data.descRoadImages.nodes.find(
+    (node) => node.relativePath == `road/desc_road_${language}.png`,
+  );
+  const descRoadImage = descRoadImageFilter
+    ? getImage(descRoadImageFilter.childImageSharp)
+    : null;
+
   return (
     <Layout>
-      <section className="max-w-6xl mx-auto md:my-5">
+      <section className="max-w-6xl mx-10 lg:mx-auto md:my-5">
         <div className="grid md:grid-flow-row-dense md:grid-cols-3">
           <div className="md:col-span-2 flex justify-center">
             <div className="grid grid-flow-row flex justify-center min-w-full">
-              <StaticImage
-                src="../images/bullecyclette.png"
-                alt="bullecyclette"
-                placeholder="blurred"
-              />
+              {bullecycletteImage && (
+                <GatsbyImage image={bullecycletteImage} alt={"bullecyclette"} />
+              )}
               <ArrowBullet
-                desc={t("balade.whatTitle")}
+                desc={t("balade.what.title")}
                 color="black"
                 bgcolor="white"
               />
               <div className="grid md:grid-cols-2 md:space-x-4">
                 <div className="text-justify whitespace-pre-line">
-                  {t("balade.whatDescription.0.text")}
+                  {t("balade.what.description.0.text")}
                 </div>
                 <div className="text-justify whitespace-pre-line">
-                  {t("balade.whatDescription.1.text")}
+                  {t("balade.what.description.1.text")}
                 </div>
               </div>
             </div>
           </div>
 
           <div className="m-auto">
-            <StaticImage
-              src={`../images/what.jpg`}
-              alt="what"
-              placeholder="blurred"
-              layout="fixed"
-              width={200}
-              height={300}
-            />
+            <Card>
+              <StaticImage
+                src={`../images/common/what.jpg`}
+                alt="what"
+                placeholder="blurred"
+                layout="fixed"
+                width={200}
+                height={300}
+              />
+            </Card>
           </div>
         </div>
       </section>
 
-      <section className="max-w-6xl mx-auto my-2 md:my-5">
+      <section className="max-w-6xl mx-10 lg:mx-auto md:my-5">
         <div className="grid md:grid-flow-row-dense md:grid-cols-3">
           <div className="m-auto">
-            <StaticImage
-              src={`../images/when.jpg`}
-              alt="what"
-              placeholder="blurred"
-              layout="constrained"
-              width={300}
-              height={500}
-            />
+            <Card>
+              <StaticImage
+                src={`../images/common/when.jpg`}
+                alt="what"
+                placeholder="blurred"
+                layout="constrained"
+                width={300}
+                height={500}
+              />
+            </Card>
           </div>
 
           <div className="md:col-span-2 flex justify-center">
             <div className="grid grid-flow-row flex justify-center min-w-full">
               <ArrowBullet
-                desc={t("balade.whenTitle")}
+                desc={t("balade.when.title")}
                 color="black"
                 bgcolor="white"
               />
 
               <StaticImage
-                src="../images/date.png"
+                src="../images/common/date_fr.png"
                 alt="date"
                 placeholder="blurred"
               />
@@ -84,13 +122,75 @@ const BaladePage: React.FC<PageProps> = () => {
         </div>
       </section>
 
-      <section className="max-w-6xl mx-auto md:my-5">
-        <PersonCard
-          alt=""
-          image={t("artists.image")}
-          title={t("artists.title")}
-          attribut="artists.persons"
-        ></PersonCard>
+      <section className="bg-blue-800">
+        <div className="max-w-6xl mx-10 lg:mx-auto pt-4">
+          <ArrowBullet
+            desc={t("balade.program.title")}
+            color="white"
+            bgcolor="black"
+          />
+        </div>
+        <div className="max-w-6xl mx-10 lg:mx-auto pt-2">
+          <GuinguetteSmallSchedule />
+        </div>
+        <div className="max-w-6xl mx-10 lg:mx-auto pt-6 pb-6">
+          <BullecycletteSchedule />
+        </div>
+      </section>
+
+      <section className="max-w-6xl mx-10 lg:mx-auto md:my-5">
+        <div className="grid md:grid-flow-row-dense md:grid-cols-2">
+          <div className="grid grid-flow-row min-w-full">
+            <ArrowBullet
+              desc={t("balade.road.title")}
+              color="black"
+              bgcolor="white"
+            />
+            <StaticImage
+              src={`../images/road.png`}
+              alt="road"
+              placeholder="blurred"
+              layout="constrained"
+              width={500}
+              height={500}
+              className="md:-mt-36"
+            />
+          </div>
+
+          <div className="flex-col">
+            <div className="w-56 h-26 flex mx-auto my-5">
+              {kmRoadImage && (
+                <GatsbyImage image={kmRoadImage} alt={"km_road"} />
+              )}
+            </div>
+            <div className="flex m-auto justify-center">
+              {stepRoadImage && (
+                <GatsbyImage image={stepRoadImage} alt={"step_road"} />
+              )}
+            </div>
+            <div className="flex m-auto justify-center">
+              {descRoadImage && (
+                <GatsbyImage image={descRoadImage} alt={"step_road"} />
+              )}
+            </div>
+            <div className="w-auto h-auto mx-auto my-2 justify-center">
+              Les cyclistes de tout genre, de tout niveau et de tout âge peuvent
+              y participer
+            </div>
+          </div>
+        </div>
+      </section>
+
+      <section className="max-w-6xl mx-10 lg:mx-auto md:my-5">
+        <Preparation />
+      </section>
+
+      <section className="max-w-6xl mx-10 lg:mx-auto md:my-5">
+        <PersonCard person="winegrowers"></PersonCard>
+      </section>
+
+      <section className="max-w-6xl mx-10 lg:mx-auto md:my-5">
+        <PersonCard person="artists"></PersonCard>
       </section>
 
       <section className="max-w-6xl mx-10 lg:mx-auto md:my-5">
@@ -160,13 +260,71 @@ export default BaladePage;
 export const Head: HeadFC = () => <title>Home Page</title>;
 
 export const query = graphql`
-  query ($language: String!) {
+  query BaladePage($language: String!) {
     locales: allLocale(filter: { language: { eq: $language } }) {
       edges {
         node {
           ns
           data
           language
+        }
+      }
+    }
+    bullecycletteImages: allFile(
+      filter: {
+        relativePath: {
+          in: ["common/bullecyclette_en.png", "common/bullecyclette_fr.png"]
+        }
+      }
+    ) {
+      nodes {
+        relativePath
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED)
+        }
+      }
+    }
+    stepRoadImages: allFile(
+      filter: {
+        relativePath: { in: ["road/step_road_en.png", "road/step_road_fr.png"] }
+      }
+    ) {
+      nodes {
+        relativePath
+        childImageSharp {
+          gatsbyImageData(
+            placeholder: BLURRED
+            layout: CONSTRAINED
+            width: 350
+            height: 400
+          )
+        }
+      }
+    }
+    kmRoadImages: allFile(
+      filter: { relativePath: { in: ["road/km_en.png", "road/km_fr.png"] } }
+    ) {
+      nodes {
+        relativePath
+        childImageSharp {
+          gatsbyImageData(
+            placeholder: BLURRED
+            layout: CONSTRAINED
+            width: 200
+            height: 100
+          )
+        }
+      }
+    }
+    descRoadImages: allFile(
+      filter: {
+        relativePath: { in: ["road/desc_road_en.png", "road/desc_road_fr.png"] }
+      }
+    ) {
+      nodes {
+        relativePath
+        childImageSharp {
+          gatsbyImageData(placeholder: BLURRED)
         }
       }
     }
