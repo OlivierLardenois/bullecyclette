e44498b93bc8a3068b28cbf3ce900d4e1495455f
Olivier Lardenois <olardenois@gmail.com>
2024-02-11 08:49:37 +0100

    fix(home): hide gallery photo on small screen

--- src/components/carousel.tsx
+++ src/components/carousel.tsx
@@ -12,12 +12,14 @@ type CarouselProps = {
 
 const Carousel: React.FC<CarouselProps> = ({ cards }) => {
   return (
-    <div className="flex flex-wrap justify-between gap-x-5 gap-y-10 [&>*]:mx-auto">
+    <div className="flex flex-wrap justify-between gap-x-5 gap-y-10 [&>*]:mx-auto [&>*:not(:first-child)]:hidden md:[&>*:not(:first-child)]:block">
       {cards.map(({ alt, image }) => {
         return (
-          <Card>
-            <GatsbyImage image={image} alt={alt} />
-          </Card>
+          <div className="first:">
+            <Card>
+              <GatsbyImage image={image} alt={alt} />
+            </Card>
+          </div>
         );
       })}
     </div>
