234e663d0414b06349bff07ff3bc1635ff8371bf
Olivier Lardenois <olardenois@gmail.com>
2024-03-07 23:38:16 +0100

    fix(guinguette): css exhibitors component

--- src/components/entityCard/exhibitors.tsx
+++ src/components/entityCard/exhibitors.tsx
@@ -13,21 +13,19 @@ const Exhibitors: React.FC = () => {
       <ArrowBullet dark>
         <h3 className="font-veteran-typewriter">{t("exhibitors.title")}</h3>
       </ArrowBullet>
-      <div className="flex flex-wrap justify-around text-justify gap-12">
+      <div className="grid grid-cols-[repeat(auto-fill,208px)] gap-6 justify-around">
         {EXHIBITORS.map(({ index, name, url }) => {
           return (
-            <div className="w-52">
-              <EntityCard title={name}>
-                <div className="space-y-4 font-veteran-typewriter grow">
-                  <p>{t(`guinguette.exhibitors.${index}`)}</p>
-                  <div className="text-white">
-                    <a href={url} target="_blank" rel="noopener noreferrer">
-                      {url}
-                    </a>
-                  </div>
+            <EntityCard title={name}>
+              <div className="space-y-4 font-veteran-typewriter grow">
+                <p>{t(`guinguette.exhibitors.${index}`)}</p>
+                <div className="text-white">
+                  <a href={url} target="_blank" rel="noopener noreferrer">
+                    {url}
+                  </a>
                 </div>
-              </EntityCard>
-            </div>
+              </div>
+            </EntityCard>
           );
         })}
       </div>
